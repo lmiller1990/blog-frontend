2597fef68c98d47b9927ee945d574551e9963a05
lachlan <lachlan.miller.1990@outlook.com>
2017-01-18 19:09:18 +0900

    Allow moxios to reponse with fake request to help with testing

--- test/unit/specs/Posts.spec.js
+++ test/unit/specs/Posts.spec.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 import Posts from 'src/components/Posts.vue'
+import moxios from 'moxios'
 
 describe('Posts.vue', () => {
+  beforeEach(function () {
+    moxios.install()
+  })
+
+  afterEach(function ()  {
+    moxios.uninstall()
+  })
+
   const vm = new Vue(Posts).$mount()
   it('renders title of posts retreived from an api', (done) => {
-    let data = vm.fetchPosts()
-    console.log(data)
+    let data = vm.fetchPosts().then(function () {
+      console.log('stuff', data)
+    }).then(done,done)
+    //console.log(data)
+
+    moxios.wait(function () {
+      moxios.requests.mostRecent().respondWith({
+        status: 200,
+        responseText: 'Post 1, Post 2, Post 3!!!!!!!!!!!!!'
+      }).then(function () {
+        console.log('moxios responded')
+      })
+    })
   })
 })
